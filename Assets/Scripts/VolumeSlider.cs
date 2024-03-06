952d9116616271023ebe24417a46539bfeda11e5
dpaz52418 <dpaz52418@gmail.com>
2024-03-05 20:07:27 -0800

    Volume Slider Implementation Part 2 / Back Button Fix (Temp. Complete)
    Fixed back button by adding Event Systems. Finished adding volume slider under settings menu in main menu. Volume slider changes volume of all sounds, including sound effects. Likely a temporary solution.

--- Assets/Scripts/VolumeSlider.cs
+++ Assets/Scripts/VolumeSlider.cs
@@ -2,28 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
+using System.Runtime.InteropServices;
 
 public class VolumeSlider : MonoBehaviour
 {
     public Slider volumeSlider;
-    private AudioSource audioSource;
+    public AudioMixer mixer;
 
+    public float minSliderValue = 0f;
+    public float maxSliderValue = 1f;
 
+    private float sliderVal;
 
-    void Start()
+    public void Start()
     {
-
-        if (PlayerPrefs.HasKey("Volume"))
-        {
-            float volume = PlayerPrefs.GetFloat("Volume");
-            volumeSlider.value = volume;
-        }
+        mixer.GetFloat("MasterVolume", out sliderVal);
+        volumeSlider.value = sliderVal;
     }
 
-    public void SetVolume(float volume)
+    public void SetVolume()
     {
-        audioSource.volume = volume;
-        PlayerPrefs.SetFloat("Volume", volume);
+        float sliderValue = volumeSlider.value;
+        if (sliderValue > -50)
+        {
+            mixer.SetFloat("MasterVolume", sliderValue);
+        }
+        else
+        {
+            mixer.SetFloat("MasterVolume", -80);
+        }
 
     }
 }
